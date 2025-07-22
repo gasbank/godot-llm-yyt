c440b7bca1cc828719ecab134619ed0781f7cb6d
Geoyeob Kim <gasbank@gmail.com>
2025-07-23 01:01:28 +0900

    건물 파괴 & 게임 오버와 재시도

--- building.gd
+++ building.gd
@@ -1,12 +1,27 @@
 extends Area2D
+class_name Building
 
 @export var hp: int = 100
 @export var max_hp: int = 100
 
+signal building_destroyed()
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	_update_hp_text()
 	
 func _on_body_entered(other: Node) -> void:
 	other.queue_free()
 	hp -= 1
+	_update_hp_text()
+	if hp <= 0:
+		queue_free()
+		var explosion := preload('res://explosion.tscn').instantiate() as GPUParticles2D
+		explosion.global_position = global_position
+		explosion.restart()
+		get_parent().add_child(explosion)
+		building_destroyed.emit()
+		#Delay.after(2.0, func(): Delay.pause())
+		
+func _update_hp_text():
 	$HPText.text = "%d/%d" % [hp, max_hp]
--- game.gd
+++ game.gd
@@ -1,14 +1,34 @@
 extends Node2D
 
 @export var enemy_scene: PackedScene
-@onready var spawn_point: Node2D = $SpawnPoint
+@export var spawn_point: Node2D
 
 func _ready() -> void:
 	# 예: 시작하자마자 5개 생성
 	spawn_multiple(5)
+	
+	for building in _collect_building_with_type(self):
+		building.building_destroyed.connect(_on_building_destroyed)
 
 func spawn_multiple(count: int) -> void:
 	for i in range(count):
 		var enemy = enemy_scene.instantiate()      # ① 프리팹 인스턴스 생성
 		enemy.position = spawn_point.position      # ② 생성 위치 지정
 		add_child(enemy)                           # ③ 씬 트리에 추가
+
+func _collect_building_with_type(root: Node) -> Array[Building]:
+	var bucket: Array[Building] = []
+	if root is Building:
+		bucket.append(root)
+	for child in root.get_children():
+		bucket.append_array(_collect_building_with_type(child))
+	return bucket
+
+func _on_building_destroyed() -> void:
+	await get_tree().create_timer(2.0).timeout
+	
+	get_tree().paused = true
+	
+	var game_over_canvas_layer := preload('res://game_over_canvas_layer.tscn').instantiate()
+	game_over_canvas_layer.process_mode = Node.PROCESS_MODE_ALWAYS
+	add_child(game_over_canvas_layer)
